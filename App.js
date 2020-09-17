b73ec883d19aba49627b837e526553a3ed17d48a
poomin12082542 <poomin12082542@hotmail.com>
2020-09-17 10:02:32 +0700

    flex box

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Weather from './components/Weather'
+
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Weather zipCode="83120" />
+    <View style={styles.container} >
+       <Weather zipCode="83120" /> 
     </View>
-   );
- }
- const styles = StyleSheet.create({
-   container:{
-     flex:1,
-     borderWidth:5,
-     borderColor:'grey',
-   }
- })
- 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderWidth: 5,
+    borderColor: 'red',    
+  }
+})
