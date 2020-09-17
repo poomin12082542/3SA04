ff9e20331921251e75ea1d757fd2fb23e232592c
poomin12082542 <poomin12082542@hotmail.com>
2020-09-17 09:47:46 +0700

    styles

--- App.js
+++ App.js
@@ -4,9 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import Weather from './components/Weather'
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Weather zipCode="83120" />
     </View>
    );
  }
+ const styles = StyleSheet.create({
+   container:{
+     flex:1,
+     borderWidth:5,
+     borderColor:'grey',
+   }
+ })
  
