768286f19222e1c011c2379fd5949e8daabdafe5
poomin12082542 <poomin12082542@hotmail.com>
2020-09-17 09:34:20 +0700

    Weather

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/Weather'
 export default function App() {
- const doIt = () => {
- console.log("Hello from console")
+  return (
+    <View>
+      <Weather />
+    </View>
+   );
  }
- return (
- <View style={styles.container}>
- <Text onPress={doIt}>Hello world</Text>
- <StatusBar style="auto" />
- </View>
- );
-}
-const styles = StyleSheet.create({
- container: {
- flex: 1,
- backgroundColor: '#fff',
- alignItems: 'center',
- justifyContent: 'center',
- },
-});
+ 
